b11549883fdadb9469491efce6312f13f2e687fd
YUVARAJ MORLA <yuvarajmorla14@gmail.com>
2023-11-06 11:49:50 +0530

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ConfigProvider } from 'antd';
+
+const theme = {
+  components: {
+    Button: {
+      colorPrimary: '#FF5733',
+      colorSecondary:"#DC143C",
+      colorPrimaryHover : '#FFBF00',
+      borderRadius: '4px'
+    }
+  },
+  token: {
+    borderRadius : '2px',
+    colorPrimary: "#673AB7",
+  },
+};
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ConfigProvider theme={theme}>
+      <App />
+    </ConfigProvider>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
