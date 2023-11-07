8c825a35442999b7ece18c46d6ce17426c928208
YUVARAJ MORLA <yuvarajmorla14@gmail.com>
2023-11-07 18:13:11 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Routes , Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
-import ProtectedPage from "./components/ProtectedPage";
+// import ProtectedPage from "./components/ProtectedPage";
 
 
 
@@ -11,7 +11,8 @@ function App() {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element= {<ProtectedPage><Home /></ProtectedPage>} />
+        {/* / <Route path="/" element= {<ProtectedPage><Home /></ProtectedPage>} />  */}
+          <Route path="/" element= {<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Home(){
     return (
-        <diV>Home</diV>
+        <diV>Home yuva</diV>
     )
 }
 
